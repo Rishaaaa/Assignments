00b423f97a3526fb2edfc6ecb7e55a3e92b59b6b
Rishaaaa <94216464+Rishaaaa@users.noreply.github.com>
2022-12-14 22:44:22 +0530

    Add files via upload

---
+++ InternPro/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+def index(request):
+    #return HttpResponse('this is home page')
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+#def contact(request):
+ #   return render(request,'contact.html')
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        contact=Contact(name=name,email=email,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request,'contact.html')
